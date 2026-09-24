6408fe3c8fa112fb37443e884812945e8bbc949d
Salman Nazeer <salmannazeer477@gmail.com>
2026-09-24 10:30:32 +0530

    Added program 7

---
+++ 07_remove_invalid_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+data = {
+    "name" : ["John", "Anna", "Nino", "Invalid Name 123", "Charlie", ""],
+    "gender" : ["Male", "Female", "Male", "Unknown", "Male", "Female"],
+    "age" : [23, 25, -1, 30, 22, 180]
+}
+
+df = pd.DataFrame(data)
+
+print("Original DataFrame with invalid data entries:")
+print(df)
+df = df[df['name'].apply(lambda x: isinstance(x, str) and x.isalpha())] 
+df = df[df['gender'].isin(['Male', 'Female'])] 
+df = df[(df['age'] >= 0) & (df['age'] <= 120)] 
+print("\nCleaned DataFrame after removing invalid entries:") 
+print(df) 
